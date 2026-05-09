44f7c390a529ea4b2b1562795be470d3219ee19d
Ali Alaei <ieee.alaei@gmail.com>
2026-05-09 15:47:33 +0330

    dsd

---
+++ examples/Pttt.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static ThemeData light() {
+    return ThemeData(brightness: Brightness.light);
+  }
+
+  static ThemeData dark() {
+    return ThemeData(brightness: Brightness.dark);
+  }
+}
